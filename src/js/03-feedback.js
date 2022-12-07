8911d394d6743abc361d920fc2db2ede489797c1
Illia <illia.tkach@isoftbet.com>
2022-12-07 21:23:08 +0200

    task 3: replaced listener from inputs to form, using json parse instead of 2 lines in local storage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,22 @@
 import throttle from 'lodash.throttle';
 
 const THROTTLE_DELAY = 500;
-const EMAIL_LOCAL_KEY = 'feedback-form-state-email';
-const MESSAGE_LOCAL_KEY = 'feedback-form-state-message';
+const DATA_LOCAL_KEY = 'feedback-form-state';
 
-const loginForm = document.querySelector('form.feedback-form');
-const emailInput = loginForm.elements.email;
-const textAreaMessage = loginForm.elements.message;
+const loginForm = document.querySelector('.feedback-form');
+const emailInput = loginForm.email;
+const messageInput = loginForm.message;
 
 setInputsValues();
 
-emailInput.addEventListener('input', throttle(inputsHandler, THROTTLE_DELAY));
-textAreaMessage.addEventListener('input', throttle(inputsHandler, THROTTLE_DELAY));
 loginForm.addEventListener('submit', onSubmit);
+loginForm.addEventListener('input', throttle(formHandler, THROTTLE_DELAY));
 
-function inputsHandler (e) {
-  /**
-   * We can use this type of check if we have just 2 inputs on form
-   */
-  const currentLocalStorageKey = e.target.name === 'email' ? EMAIL_LOCAL_KEY : MESSAGE_LOCAL_KEY;
+function formHandler () {
+  const email = emailInput.value;
+  const message = messageInput.value;
 
-  localStorage[currentLocalStorageKey] = e.target.value;
+  localStorage[DATA_LOCAL_KEY] = JSON.stringify({email, message});
 }
 
 function onSubmit (e) {
@@ -28,20 +24,17 @@ function onSubmit (e) {
 
   console.log({
     [emailInput.name] : emailInput.value,
-    [textAreaMessage.name] : textAreaMessage.value
+    [messageInput.name] : messageInput.value
   });
 
   loginForm.reset();
-  localStorage[EMAIL_LOCAL_KEY] = '';
-  localStorage[MESSAGE_LOCAL_KEY] = '';
+  localStorage[DATA_LOCAL_KEY] = '';
 }
 
 function setInputsValues () {
-  if (localStorage[EMAIL_LOCAL_KEY]) {
-    emailInput.value = localStorage[EMAIL_LOCAL_KEY];
-  }
+  if (!localStorage[DATA_LOCAL_KEY]) return;
+  const {email, message} = JSON.parse(localStorage[DATA_LOCAL_KEY]);
 
-  if (localStorage[MESSAGE_LOCAL_KEY]) {
-    textAreaMessage.value = localStorage[MESSAGE_LOCAL_KEY];
-  }
+  if (email) emailInput.value = email;
+  if (message) messageInput.value = message;
 }
